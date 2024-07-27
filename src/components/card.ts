743dc3efa798c16d2750b477825ae24d6634853d
Agata Ordano <gh@choccymilk.dev>
2024-07-27 13:48:09 -0300

    feat(components): add daisy card component

---
+++ src/components/card.ts
@@ -0,0 +1,34 @@
+import { html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { BaseElement } from "./base";
+
+@customElement("choccy-card")
+export class Card extends BaseElement {
+    @property({ type: String }) title = "";
+    @property({ type: String }) image = "";
+
+    render() {
+        return html`
+            <div class="card w-96 bg-base-100 shadow-xl">
+                ${this.image
+                    ? html`<figure class="h-80">
+                          <img src="${this.image}" alt="${this.title}" />
+                      </figure>`
+                    : null}
+                <div class="card-body">
+                    <h2 class="card-title">${this.title}</h2>
+                    <slot></slot>
+                    <div class="card-actions justify-end">
+                        <slot name="actions"></slot>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "choccy-card": Card;
+    }
+}
