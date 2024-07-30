239bfc23abd45c6f401f9143b30e841b8a801a87
Agata Ordano <gh@choccymilk.dev>
2024-07-30 20:22:35 -0300

    fix(site): fix build system to include static html files

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import FullReload from "vite-plugin-full-reload";
 export default defineConfig({
     root: "vite",
     publicDir: path.resolve(__dirname, "static"),
-    plugins: [symlink(), FullReload(["vite/**/*.html"])],
+    plugins: [symlink(), symlinkHTML(), FullReload(["vite/**/*.html"])],
     build: {
         outDir: "../dist",
         emptyOutDir: true,
@@ -28,7 +28,61 @@ function symlink() {
             try {
                 fs.symlinkSync(src, viteSrc, "junction");
             } catch (err) {
-                console.error(`Error creating symlink: ${err.message}`);
+                if (err.code !== "EEXIST") {
+                    console.error(`Error creating symlink: ${err.message}`);
+                }
+            }
+        },
+    };
+}
+
+function symlinkHTML() {
+    return {
+        name: "symlink",
+        buildStart() {
+            const viteRoot = path.resolve(__dirname, "vite");
+            const staticDir = path.resolve(__dirname, "static");
+
+            try {
+                if (!fs.existsSync(staticDir)) {
+                    fs.mkdirSync(staticDir, { recursive: true });
+                }
+
+                const walkSync = (dir, callback) => {
+                    fs.readdirSync(dir).forEach((file) => {
+                        const filePath = path.join(dir, file);
+                        if (fs.statSync(filePath).isDirectory()) {
+                            walkSync(filePath, callback);
+                        } else if (file.endsWith(".html")) {
+                            callback(filePath, file);
+                        }
+                    });
+                };
+
+                walkSync(viteRoot, (filePath, file) => {
+                    const relativePath = path.relative(viteRoot, filePath);
+                    const targetPath = path.join(staticDir, relativePath);
+
+                    const targetDir = path.dirname(targetPath);
+                    if (!fs.existsSync(targetDir)) {
+                        fs.mkdirSync(targetDir, { recursive: true });
+                    }
+
+                    try {
+                        if ((file = "index.html")) {
+                            return;
+                        }
+                        fs.symlinkSync(filePath, targetPath, "file");
+                    } catch (err) {
+                        if (err.code !== "EEXIST") {
+                            console.error(
+                                `Error creating symlink: ${err.message}`
+                            );
+                        }
+                    }
+                });
+            } catch (err) {
+                console.error(`Error creating symlinks: ${err.message}`);
             }
         },
     };
